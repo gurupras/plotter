61f3624146306fef64cb084a7d86baea6b790476
Guru Prasad <gurupras@buffalo.edu>
2013-09-08 00:22:43 -0400

    Updated Plotter     - Added command-line option 'window_size' for moving average         - Added logic to smoothen as well     - Renamed x_axis_raw and y_axis_raw to x_axis and y_axis     - Now saves svg and png

--- src/Plotter.py
+++ src/Plotter.py
@@ -10,22 +10,24 @@ import numpy
 import matplotlib.pyplot as plt;
 import re
 
+
 def setup_parser(parser) :
-    parser.add_option("-f", "--file", type="string" , action="store", help="Input file to parse")
-    parser.add_option("-x", "--x_axis", type="string" , action="store", help="X-Axis name")
-    parser.add_option("-y", "--y_axis", type="string" , action="store", help="Y-Axis name")
-    parser.add_option("-o", "--output", type="string" , action="store", help="File in which resulting plot is to be stored")
-    parser.add_option("--x_conversion", type="string" , action="store", help="Apply a formula on each value of the X-Axis")
-    parser.add_option("--y_conversion", type="string" , action="store", help="Apply a formula on each value of the Y-Axis")
+    parser.add_option("-f", "--file"  , type="string", action="store", help="Input file to parse")
+    parser.add_option("-x", "--x_axis", type="string", action="store", help="X-Axis name")
+    parser.add_option("-y", "--y_axis", type="string", action="store", help="Y-Axis name")
+    parser.add_option("-o", "--output", type="string", action="store", help="File in which resulting plot is to be stored")
+    parser.add_option("--x_conversion", type="string", action="store", help="Apply a formula on each value of the X-Axis")
+    parser.add_option("--y_conversion", type="string", action="store", help="Apply a formula on each value of the Y-Axis")
+    parser.add_option("--window_size" , type="string", action="store", help="Window size to use for moving average")
 
 def check_options(options) :
    
     assert options.file != None, "Must specify input file to parse!"
         
     if options.x_conversion != None :
-        assert options.x_axis != None, "Must specify x_axis when specifying x_conversion"
+        assert options.x_axis != None, "Must specify x_axis_raw when specifying x_conversion"
     if options.y_conversion != None :
-        assert options.y_axis != None, "Must specify y_axis when specifying y_conversion"
+        assert options.y_axis != None, "Must specify y_axis_raw when specifying y_conversion"
 
 
 def convert(value, logic) :
@@ -40,6 +42,26 @@ def convert(value, logic) :
     if match_obj.group(1) is "/" :
         return float(float(value) / float(match_obj.group(2)))
 
+def smoothen(x_axis_raw, y_axis_raw, window_size) :
+    x_axis_sum  = 0
+    y_axis_sum  = 0
+    
+    x_axis      = []
+    y_axis      = []
+    
+    for index in range(len(x_axis_raw)) :
+        if index > window_size :
+            x_axis.append(x_axis_sum / window_size)
+            y_axis.append(y_axis_sum / window_size)
+            
+            x_axis_sum = x_axis_sum - x_axis_raw[index - window_size]
+            y_axis_sum - y_axis_sum - y_axis_raw[index - window_size]
+
+        x_axis_sum  = x_axis_sum + x_axis_raw[index]
+        y_axis_sum  = y_axis_sum + y_axis_raw[index]
+        
+    return x_axis, y_axis
+            
 if __name__ == '__main__':
     parser = optparse.OptionParser()
     setup_parser(parser)
@@ -50,24 +72,30 @@ if __name__ == '__main__':
     
     input_file = open(options.file, "r")
     
-    x_axis = []
-    y_axis = []
+    x_axis_raw = []
+    y_axis_raw = []
     for line in input_file :
         split = line.split('=', 1)
         if len(split) < 2 :
             print 'Warning: Ignored line :\n' + line + '\n'
             continue
         
-        x_val = split[0]
-        y_val = split[1]
+        x_val = float(split[0])
+        y_val = float(split[1])
         
         if options.x_conversion != None :
             x_val = convert(x_val, options.x_conversion)
         if options.y_conversion != None :
             y_val = convert(y_val, options.y_conversion)
         
-        x_axis.append(x_val)
-        y_axis.append(y_val)
+        x_axis_raw.append(x_val)
+        y_axis_raw.append(y_val)
+    
+    x_axis = x_axis_raw
+    y_axis = y_axis_raw
+    
+    if options.window_size != None :
+        (x_axis, y_axis) = smoothen(x_axis_raw, y_axis_raw, int(options.window_size))
     
     plt.plot(x_axis, y_axis)
     
@@ -80,5 +108,6 @@ if __name__ == '__main__':
     
     if options.output != None :
         plt.savefig(options.output + '.svg')
+        plt.savefig(options.output + '.png')
         
     plt.show()
