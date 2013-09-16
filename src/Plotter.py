8cec0b79d948e0ca6b8d54bd3d273712d4263604
Guru Prasad <gurupras@buffalo.edu>
2013-09-16 17:33:36 -0400

    Plotter.py now uses argparse instead of optparse
    Created separate functional units for each process in Plotter
    
    Added Plotter.multiplot()

--- src/Plotter.py
+++ src/Plotter.py
@@ -5,32 +5,34 @@ Created on Sep 6, 2013
 '''
 
 import sys
-import optparse
+import argparse
 import numpy
 import matplotlib.pyplot as plt;
 import re
 import operator
 
 def setup_parser(parser) :
-    parser.add_option("-f", "--file"  , type="string", action="store", help="Input file to parse")
-    parser.add_option("-x", "--x_axis", type="string", action="store", help="X-Axis name")
-    parser.add_option("-y", "--y_axis", type="string", action="store", help="Y-Axis name")
-    parser.add_option("-o", "--output", type="string", action="store", help="File in which resulting plot is to be stored")
-    parser.add_option("--x_conversion", type="string", action="store", help="Apply a formula on each value of the X-Axis")
-    parser.add_option("--y_conversion", type="string", action="store", help="Apply a formula on each value of the Y-Axis")
-    parser.add_option("--window_size" , type="string", action="store", help="Window size to use for moving average")
+    parser.add_argument('-f', "--file"  , type=str, help="Input file to parse_args")
+    parser.add_argument("-x", "--x_axis", type=str, help="X-Axis name")
+    parser.add_argument("-y", "--y_axis", type=str, help="Y-Axis name")
+    parser.add_argument("-o", "--output", type=str, help="File in which resulting plot is to be stored")
+    parser.add_argument("--x_conversion", type=str, help="Apply a formula on each value of the X-Axis")
+    parser.add_argument("--y_conversion", type=str, help="Apply a formula on each value of the Y-Axis")
+    parser.add_argument("--window_size" , type=str, help="Window size to use for moving average")
+    parser.add_argument("--legend"      , type=str, help="Plot legend while using multiplot")
 
-def check_options(options) :
+def check_args(args) :
    
-    assert options.file != None, "Must specify input file to parse!"
+    assert args.file != None, "Must specify input file to parse_args!"
         
-    if options.x_conversion != None :
-        assert options.x_axis != None, "Must specify x_axis_raw when specifying x_conversion"
-    if options.y_conversion != None :
-        assert options.y_axis != None, "Must specify y_axis_raw when specifying y_conversion"
+    if args.x_conversion != None :
+        assert args.x_axis != None, "Must specify x_axis_raw when specifying x_conversion"
+    if args.y_conversion != None :
+        assert args.y_axis != None, "Must specify y_axis_raw when specifying y_conversion"
 
 
 def convert(value, logic) :
+#     logic = logic.strip("\"'")
     match_obj = re.match("([-/+*])\s*(\d+e?\d+)", logic)
     
     if match_obj.group(1) is "+" :
@@ -67,18 +69,11 @@ def smoothen(plot_points, window_size) :
             y_axis_sum = y_axis_sum - sorted_output_raw[index - window_size + 1][1]
         
     return x_axis, y_axis
-            
-if __name__ == '__main__':
-    parser = optparse.OptionParser()
-    setup_parser(parser)
-    
-    (options, args) = parser.parse_args()
-    
-    check_options(options)
-    
-    input_file = open(options.file, "r")
-    
+
+
+def parse_data(args) :
     plot_points = {}
+    input_file = open(args.file, "r")
     for line in input_file :
         split = line.split('=', 1)
         if len(split) < 2 :
@@ -88,30 +83,72 @@ if __name__ == '__main__':
         x_val = float(split[0])
         y_val = float(split[1])
         
-        if options.x_conversion != None :
-            x_val = convert(x_val, options.x_conversion)
-        if options.y_conversion != None :
-            y_val = convert(y_val, options.y_conversion)
+        if args.x_conversion != None :
+            x_val = convert(x_val, args.x_conversion)
+        if args.y_conversion != None :
+            y_val = convert(y_val, args.y_conversion)
 
         plot_points.update({x_val: y_val})
-    
+    return plot_points
+
+def plot(plot_points, args, label=None) :
     x_axis = plot_points.keys()
     y_axis = plot_points.values()
-    if options.window_size != None :
-        (x_axis, y_axis) = smoothen(plot_points, int(options.window_size))
+    if args.window_size != None :
+        (x_axis, y_axis) = smoothen(plot_points, int(args.window_size))
     
-    plt.plot(x_axis, y_axis)
-    plt.axis([min(x_axis), max(x_axis) + min(x_axis), min(y_axis), max(y_axis) + min(y_axis)])
+    plt.plot(x_axis, y_axis, label=label)
+    plt.axis([0, max(x_axis) + min(x_axis), 0, max(y_axis) + min(y_axis)])
     
-    if options.x_axis != None :
-        plt.xlabel(options.x_axis)
-    if options.y_axis != None :
-        plt.ylabel(options.y_axis)
+    if args.x_axis != None :
+        plt.xlabel(args.x_axis)
+    if args.y_axis != None :
+        plt.ylabel(args.y_axis)
     
     plt.grid(True)
-    
-    if options.output != None :
-        plt.savefig(options.output + '.svg')
-        plt.savefig(options.output + '.png')
-        
+
+
+def save(args) :
+    if args.output != None :
+        plt.savefig(args.output + '.svg')
+        plt.savefig(args.output + '.png')    
+
+
+def show() :
     plt.show()
+
+
+def parse_args(argv=None) :
+    if argv is None :
+        argv = sys.argv[1:]
+    
+    parser = argparse.ArgumentParser()
+    setup_parser(parser)
+    
+    args = parser.parse_args(args=argv)
+    
+    check_args(args)
+    return args
+
+def multiplot(argv) :
+    args = parse_args(argv)
+#     args.file = args.file.strip("\"'")
+    file_list = args.file.split(",")
+    for input_file in file_list :
+        args.file = input_file
+        plot_points = parse_data(args)
+        index = file_list.index(input_file)
+        label = str(index + 1) + " " + args.legend
+        plot(plot_points, args, label)
+    plt.legend(loc=2)
+    save(args)
+    plt.clf()
+        
+if __name__ == '__main__':
+    args = parse_args()
+    
+    plot_points = parse_data(args)
+
+    plot(plot_points, args)
+    save(args)
+    show()
